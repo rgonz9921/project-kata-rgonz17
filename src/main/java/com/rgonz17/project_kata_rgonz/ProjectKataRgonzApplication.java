edd5246bfed526cad6a67bff7fd626446c67f049
Gonzalez, Andres <rgonz17@bancodebogota.com.co>
2025-03-20 13:10:24 -0500

    add base project

---
+++ src/main/java/com/rgonz17/project_kata_rgonz/ProjectKataRgonzApplication.java
@@ -0,0 +1,13 @@
+package com.rgonz17.project_kata_rgonz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectKataRgonzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectKataRgonzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rgonz17/project_kata_rgonz/ProjectKataRgonzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rgonz17.project_kata_rgonz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectKataRgonzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
