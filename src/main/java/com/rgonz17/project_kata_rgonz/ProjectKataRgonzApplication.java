c0d1f91d813f7515b018088be3d60ae85a08826c
Gonzalez, Andres <rgonz17@bancodebogota.com.co>
2025-03-20 19:29:43 -0500

    add jwt and user

--- src/main/java/com/rgonz17/project_kata_rgonz/ProjectKataRgonzApplication.java
+++ src/main/java/com/rgonz17/project_kata_rgonz/ProjectKataRgonzApplication.java
@@ -2,11 +2,15 @@ package com.rgonz17.project_kata_rgonz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class ProjectKataRgonzApplication {
-
 	public static void main(String[] args) {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String hashedPassword = passwordEncoder.encode("123456");
+		System.out.println("Contraseña encriptada: " + hashedPassword);
 		SpringApplication.run(ProjectKataRgonzApplication.class, args);
 	}
 
